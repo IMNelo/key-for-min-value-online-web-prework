36e5cd52c9dcd9c7196200796f08f10ce607b663
Agnelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-02-07 07:33:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  a <= 1
-  b > 1
 hash.each do |key, value|
-
     puts value 
-  
- 
   end
 end
 
