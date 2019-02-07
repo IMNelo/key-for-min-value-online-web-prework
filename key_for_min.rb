91c607dc0eb9fe2ffc2bf10fe621dc55f18f5e89
Agnelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-02-07 07:23:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,12 +6,3 @@ hash.each do |key, value|
   value 
 end
 
-describe "smallest hash value" do
-
-  it "does not call the `#keys` method" do
-    hash = {:blake => 500, :ashley => 2, :adam => 1}
-
-    expect(hash).to_not receive(:keys)
-
-    key_for_min_value(hash)
-  end
